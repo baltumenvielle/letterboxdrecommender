be14b1292f4900c406a73cb290c23e39fbe31f8e
Baltazar Menvielle <60141792+baltumenvielle@users.noreply.github.com>
2025-04-12 01:50:10 -0300

    Delete scraping directory

--- scraping/imbdscraper/imbdscraper/limpiar_es.py
+++
@@ -1,7 +0,0 @@
-with open("urls_peliculas.txt", "r") as f:
-    urls = f.readlines()
-
-urls_limpias = [url.replace("/es/", "/") for url in urls]
-
-with open("urls_peliculas.txt", "w") as f:
-    f.writelines(urls_limpias)
